57b034c154fc64baa9cf42a40b123fd44a993256
Jazmine Singh <jzmn65@gmail.com>
2025-08-29 14:23:13 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jazmine Singh on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FootballTests/FootballTests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballTests.swift
+//  FootballTests
+//
+//  Created by Jazmine Singh on 8/29/25.
+//
+
+import Testing
+@testable import Football
+
+struct FootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
